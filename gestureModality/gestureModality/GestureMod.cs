3efe30e58cada34d871a7a8ae219ca50edad30a9
Pedro <pedrogusmao@ua.pt>
2018-05-17 00:03:42 +0100

    gesturemod

---
+++ gestureModality/gestureModality/GestureMod.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mmisharp;
+using Microsoft.Kinect;
+
+namespace gestureModality
+{
+    public class GestureMod
+    {
+        private LifeCycleEvents lce;
+        private MmiCommunication mmic;
+
+        public GestureMod()
+        {
+            //init LifeCycleEvents..
+            lce = new LifeCycleEvents("GESTURES", "FUSION", "gesture-1", "acoustic", "command"); // LifeCycleEvents(string source, string target, string id, string medium, string mode)
+            mmic = new MmiCommunication("localhost", 8000, "User2", "GESTURES"); // MmiCommunication(string IMhost, int portIM, string UserOD, string thisModalityName)
+
+            mmic.Send(lce.NewContextRequest());
+
+        }
+
+        private void GestureRecognized(String gestureName, float confidence)
+        {
+            //SEND
+            // IMPORTANT TO KEEP THE FORMAT {"recognized":["SHAPE","COLOR"]}
+            string json = "{ \"recognized\": [";
+            json += "\"" + gestureName + "\", ";
+            json = json.Substring(0, json.Length - 2);
+            json += "] }";
+            Console.WriteLine(json);
+
+            var exNot = lce.ExtensionNotification("", "", confidence, json);
+            mmic.Send(exNot);
+        }
+
+    }
+}
