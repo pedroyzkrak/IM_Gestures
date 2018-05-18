62e4eedfaa40607a3d28f4ae6b349f56633d8f80
Pedro <pedrogusmao@ua.pt>
2018-05-18 03:19:02 +0100

    test #2

--- gestureModality/gestureModality/MainWindow.xaml.cs
+++ gestureModality/gestureModality/MainWindow.xaml.cs
@@ -11,45 +11,28 @@ namespace gestureModality
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        
+
         private KinectSensor kinect = null;
-        
+
         private string statusText = null;
-        /*
+
         private GestureMod _gm;
         private VisualGestureBuilderDatabase vgbDb;
         private VisualGestureBuilderFrameSource vgbFrameSource;
         private VisualGestureBuilderFrameReader vgbFrameReader;
-        private BodyFrameReader bodyFrameReader; 
-        */
+        private BodyFrameReader bodyFrameReader;
+
         public MainWindow()
         {
             InitializeComponent();
-            
-            
-            //this.DataContext = this;
-            /*
-            _gm = new GestureMod();
-            bodyFrameReader = kinect.BodyFrameSource.OpenReader();
-            bodyFrameReader.FrameArrived += bodyFrameArrived;
-            vgbDb = new VisualGestureBuilderDatabase(@"..\Gestures\Gesture.gbd");
-            vgbFrameSource = new VisualGestureBuilderFrameSource(KinectSensor.GetDefault(), 0);
-            foreach (var g in vgbDb.AvailableGestures)
-            {
-                vgbFrameSource.AddGesture(g);
-            }
-            vgbFrameReader = vgbFrameSource.OpenReader();
-            vgbFrameReader.FrameArrived += vgbFrameArrived; 
-            */
-            
-            
+
         }
         public event PropertyChangedEventHandler PropertyChanged;
-        
+
         private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
         {
             this.StatusText = this.kinect.IsAvailable ? "The Kinect is availabe." : "Kinect is Unavailable!";
-        } 
+        }
         public string StatusText
         {
             get
@@ -82,82 +65,92 @@ namespace gestureModality
             this.kinect = KinectSensor.GetDefault();
             this.kinect.IsAvailableChanged += this.Sensor_IsAvailableChanged;
             this.StatusText = this.kinect.IsAvailable ? "The Kinect is availabe." : "Kinect is Unavailable!";
+            this.DataContext = this;
+            _gm = new GestureMod();
+            bodyFrameReader = kinect.BodyFrameSource.OpenReader();
+            bodyFrameReader.FrameArrived += bodyFrameArrived;
+            vgbDb = new VisualGestureBuilderDatabase(@"..\Gestures\Gesture.gbd");
+            vgbFrameSource = new VisualGestureBuilderFrameSource(KinectSensor.GetDefault(), 0);
+            foreach (var g in vgbDb.AvailableGestures)
+            {
+                vgbFrameSource.AddGesture(g);
+            }
+            vgbFrameReader = vgbFrameSource.OpenReader();
+            vgbFrameReader.FrameArrived += vgbFrameArrived;
             kinect.Open();
         }
-        /*
-private void bodyFrameArrived(object sender, BodyFrameArrivedEventArgs e)
-{
-   if (!vgbFrameSource.IsTrackingIdValid)
-   {
-       using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
-       {
-           if (bodyFrame != null)
-           {
-               Body[] bodies = new Body[6];
-               bodyFrame.GetAndRefreshBodyData(bodies);
-               Body closestBody = null;
-               foreach (Body b in bodies)
-               {
-                   if (b.IsTracked)
-                   {
-                       if (closestBody == null)
-                       {
-                           closestBody = b;
-                       }
-                       else
-                       {
-                           Joint newHeadJoint = b.Joints[JointType.Head];
-                           Joint oldHeadJoint = closestBody.Joints[JointType.Head];
-                           if (newHeadJoint.TrackingState == TrackingState.Tracked &&
-                          newHeadJoint.Position.Z < oldHeadJoint.Position.Z)
-                           {
-                               closestBody = b;
-                           }
 
-                       }
-                   }
-                   if (closestBody != null)
-                   {
-                       vgbFrameSource.TrackingId = closestBody.TrackingId;
-                   }
-               }
-           }
-       }
-   }
-}
+        private void bodyFrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            if (!vgbFrameSource.IsTrackingIdValid)
+            {
+                using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+                {
+                    if (bodyFrame != null)
+                    {
+                        Body[] bodies = new Body[6];
+                        bodyFrame.GetAndRefreshBodyData(bodies);
+                        Body closestBody = null;
+                        foreach (Body b in bodies)
+                        {
+                            if (b.IsTracked)
+                            {
+                                if (closestBody == null)
+                                {
+                                    closestBody = b;
+                                }
+                                else
+                                {
+                                    Joint newHeadJoint = b.Joints[JointType.Head];
+                                    Joint oldHeadJoint = closestBody.Joints[JointType.Head];
+                                    if (newHeadJoint.TrackingState == TrackingState.Tracked &&
+                                   newHeadJoint.Position.Z < oldHeadJoint.Position.Z)
+                                    {
+                                        closestBody = b;
+                                    }
 
-private void vgbFrameArrived(object sender, VisualGestureBuilderFrameArrivedEventArgs e)
-{
-   using (var vgbFrame = e.FrameReference.AcquireFrame())
-   {
-       if (vgbFrame != null && vgbFrame.DiscreteGestureResults != null)
-       {
-           var discreteResults = vgbFrame.DiscreteGestureResults;
-           if (discreteResults != null)
-           {
-               foreach (Gesture g in vgbFrameSource.Gestures)
-               {
-                   if (g.GestureType == GestureType.Discrete)
-                   {
-                       DiscreteGestureResult result = null;
-                       discreteResults.TryGetValue(g, out result);
+                                }
+                            }
+                            if (closestBody != null)
+                            {
+                                vgbFrameSource.TrackingId = closestBody.TrackingId;
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-                       if (result != null)
-                       {
-                           Console.WriteLine(g.Name + " " + result.Confidence + "\n");
-                       }
-                   }
-               }
-           }
-           else
-           {
-               //cenas ? 
-           }
-       }
-   }
-}
+        private void vgbFrameArrived(object sender, VisualGestureBuilderFrameArrivedEventArgs e)
+        {
+            using (var vgbFrame = e.FrameReference.AcquireFrame())
+            {
+                if (vgbFrame != null && vgbFrame.DiscreteGestureResults != null)
+                {
+                    var discreteResults = vgbFrame.DiscreteGestureResults;
+                    if (discreteResults != null)
+                    {
+                        foreach (Gesture g in vgbFrameSource.Gestures)
+                        {
+                            if (g.GestureType == GestureType.Discrete)
+                            {
+                                DiscreteGestureResult result = null;
+                                discreteResults.TryGetValue(g, out result);
 
+                                if (result != null)
+                                {
+                                    Console.WriteLine(g.Name + " " + result.Confidence + "\n");
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //cenas ? 
+                    }
+                }
+            }
+        }
 
-*/
     }
 }
